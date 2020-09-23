8e2a27d4d05093652d816f87d31625c2fa47aef6
student <student@158.197.35.241>
2020-09-23 11:07:10 +0200

    Gold Experience Requiem

--- src/paz1c2020_githubos_testos/what.java
+++ src/paz1c2020_githubos_testos/what.java
@@ -3,8 +3,10 @@ package paz1c2020_githubos_testos;
 public class what {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		for(int i = 0; i < 50; i++) {
+			System.out.println("Wha-");
+		}
+		System.out.println("This... is Requiem.");
 	}
 
 }
