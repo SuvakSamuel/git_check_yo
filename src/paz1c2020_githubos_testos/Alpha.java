fee28755ec3bb90787ef5d1b3923364a28c4a123
student <student@158.197.35.241>
2020-09-23 11:32:04 +0200

    editos

--- src/paz1c2020_githubos_testos/Alpha.java
+++ src/paz1c2020_githubos_testos/Alpha.java
@@ -4,6 +4,7 @@ public class Alpha {
 	
 	public static void main(String[] args) {
 		System.out.println("Samuel Suvak has already touched this code");
+		System.out.println("Killer Queen, the Third Bomb: Bites the Dust!");
 		
 	}
 
