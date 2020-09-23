df75ca8090636d2cf3c954d322577046cebe196d
student <student@158.197.35.241>
2020-09-23 11:38:47 +0200

    yet another fcukign edit

--- src/paz1c2020_githubos_testos/Alpha.java
+++ src/paz1c2020_githubos_testos/Alpha.java
@@ -6,6 +6,8 @@ public class Alpha {
 		System.out.println("Samuel Suvak has already touched this code");
 		System.out.println("Killer Queen, the Third Bomb: Bites the Dust!");
 		
+		System.out.println("Domino Madzik, if you are reading this then go commit toaster bath xddddd");
+		
 	}
 
 }
