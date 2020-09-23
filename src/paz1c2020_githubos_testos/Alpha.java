53340e0de840efc69bf956d6afb6a797c0e80be3
student <student@158.197.35.241>
2020-09-23 11:13:15 +0200

    My name Jeff

--- src/paz1c2020_githubos_testos/Alpha.java
+++ src/paz1c2020_githubos_testos/Alpha.java
@@ -3,10 +3,7 @@ package paz1c2020_githubos_testos;
 public class Alpha {
 	
 	public static void main(String[] args) {
-		
-		for(int i = 0; i < 20; i++) {
-			System.out.println("KINGU CRIMSON");
-		}
+		System.out.println("Samuel Suvak has already touched this code");
 		
 	}
 
